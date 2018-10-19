2798b7bf7d7a6f1fb387f232b7390a8826397bc4
rkritchat <rkritchat@gmail.com>
2018-10-19 10:20:00 +0700

    Test fetch from git

--- src/TestTools.java
+++ src/TestTools.java
@@ -2,7 +2,6 @@ public class TestTools {
     public static void main(String... args) {
         System.out.println("Test");
         testCommit();
-        System.out.println("113");
     }
 
     private static void testCommit() {
