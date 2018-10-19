cd6ef2581e7b762bef5713686e112717b6629bc5
rkritchat <rkritchat@gmail.com>
2018-10-19 10:00:35 +0700

    Test revert branch

--- src/TestTools.java
+++ src/TestTools.java
@@ -2,6 +2,7 @@ public class TestTools {
     public static void main(String... args) {
         System.out.println("Test");
         testCommit();
+        System.out.println("11");
     }
 
     private static void testCommit() {
