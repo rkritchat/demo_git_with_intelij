3507224c676a01987dc3b0567e8f7e008a1269a1
rkritchat <rkritchat@gmail.com>
2018-10-19 09:55:09 +0700

    Test commit and push file from inteliJ

--- src/TestTools.java
+++ src/TestTools.java
@@ -1,5 +1,10 @@
 public class TestTools {
     public static void main(String... args) {
         System.out.println("Test");
+        testCommit();
+    }
+
+    private static void testCommit() {
+        System.out.println("test commit");
     }
 }
