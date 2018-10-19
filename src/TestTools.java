f92db9b076d4b1ed1a2d6ecff5fafcb75da1dc01
rkritchat <rkritchat@gmail.com>
2018-10-19 10:26:26 +0700

    Create conflict file and test

--- src/TestTools.java
+++ src/TestTools.java
@@ -1,6 +1,7 @@
 public class TestTools {
     public static void main(String... args) {
         testCommit();
+        System.out.println("Done by test1");
     }
 
     private static void testCommit() {
