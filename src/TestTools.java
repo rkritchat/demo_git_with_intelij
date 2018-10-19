d2adb456928092fa2d49363dcf8f9c14d3e080fe
rkritchat <rkritchat@gmail.com>
2018-10-19 10:14:22 +0700

    Try to create conflict file

--- src/TestTools.java
+++ src/TestTools.java
@@ -2,7 +2,7 @@ public class TestTools {
     public static void main(String... args) {
         System.out.println("Test");
         testCommit();
-        System.out.println("112");
+        System.out.println("113");
     }
 
     private static void testCommit() {
